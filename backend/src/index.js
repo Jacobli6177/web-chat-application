b552dd0e6a9c2a017f14021d2fd50c0b7babba79
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-11 21:45:56 -0400

    setting up database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express"
+import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route"
 
+dotenv.config()
+
 const app = express();
+
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes)
 
