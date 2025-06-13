d1827c6c0e12923a6791ad2120eafe698f47f219
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-12 22:58:28 -0400

    implying password credientials

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,22 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import User from "../models/user.model"
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        if(password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 character"});
+
+        } 
+
+        const user = await User.findOne({email})
+
+        if (user) return res.status(400).json({ message : "Email already Exist"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password.salt);
+    } catch (error) {
+
+    }
 };
 
 export const login = (req, res) => {
