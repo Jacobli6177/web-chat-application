c3e014384a54c31bac739df766ca1d6a6a8c986c
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-18 21:36:40 -0400

    imported tailwind

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,5 +22,6 @@ app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT)
-    connectDB(); // <-- Now defined
+    connectDB(); 
+    
 })
