2548c0cea75c4fedd1d545c3f5362a0516d804c7
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-20 22:34:51 -0400

    updating routes for pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "../src/routes/auth.route.js"
 import messageRoutes from "../src/routes/message.route.js"
-
+import cors from "cors";
 import connectDB from '../src/lib/db.js'
 import cookieParser from "cookie-parser"
 import { connectDB } from "../src/lib/db.js"
@@ -15,6 +15,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser);
+app.use(cors({
+    origin: "http://localhost:5173",
+    credential: true,
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useAuthStore } from "../store/useAuthStore";
 
 const NavBar = () => {
+  const {authUser} = useAuthStore();
+
   return (
     <div>
-      <NavBar />
+      
     </div>
   )
 }
